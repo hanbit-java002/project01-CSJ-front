e76855ca06b12f9a5b4be4c46eedb1397e4ccfcd
Cyphers <wntjdco92@naver.com>
2017-02-21 18:21:30 +0900

    20170220 연동 페이지 추가

--- src/js/article.notice.js
+++ src/js/article.notice.js
@@ -4,5 +4,19 @@
 require([
 	"common",
 ], function() {
-
+	$(".notice-menu>ul>li:first-child").on("click", function() {
+		location.href = global.root + "/article/article.notice.html";
+	});
+	$(".notice-menu>ul>li:nth-child(2)").on("click", function() {
+		location.href = global.root + "/article/update.html";
+	});
+	$(".notice-menu>ul>li:nth-child(3)").on("click", function() {
+		location.href = global.root + "/page/eclipse.html";
+	});
+	$(".notice-menu>ul>li:nth-child(4)").on("click", function() {
+		location.href = global.root + "/article/magazine.html";
+	});
+	$(".notice-menu>ul>li:nth-child(5)").on("click", function() {
+		location.href = global.root + "/article/event/running.html";
+	});
 });
