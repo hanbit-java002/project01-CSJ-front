c766131bbe2d0f8b78c137162727391dd2fa3097
Cyphers <wntjdco92@naver.com>
2017-03-17 17:57:26 +0900

    20170317 기능 주기 병행

--- src/js/game-info.js
+++ src/js/game-info.js
@@ -16,4 +16,29 @@ require([
 	$(".notice-menu>ul>li:last-child").on("click", function() {
 		location.href = global.root + "/game/item-box.html";
 	});
+	function charcterthum() {
+		var charname = ["loras", "huton", "louis", "tara", "trivia", "cain",
+			"rena", "drexler", "doyle", "thomas", "niobe", "shiva", "wesley",
+			"stella", "alicia", "clare", "deimus", "eagle", "marlene", "charlotte",
+			"willard", "lleyton", "michelle", "rin", "viktor", "carlos",
+			"hotaru", "trixie", "ricardo", "camille", "jannette", "peter", "issac",
+			"rebecca", "ellie", "martin", "bruce", "mia", "denise",
+			"gereon", "lucy", "tian", "harang", "j", "belzer", "stormshadow",
+			"richel", "risa", "rick", "jekiel", "tanya", "carol",
+			"lysander", "ludwig", "melvin", "diana", "clive", "helena", "eva",
+			"empty", "empty", "empty", "empty", "empty", "empty"];
+
+		for(var i=0; i<charname.length; i++) {
+			var char = charname[i];
+			var imgHTML = "<li>";
+			imgHTML += "<a href='#'>";
+			imgHTML += "<img src='../img/thum_name_on_" + char + ".jpg'>";
+			imgHTML += "</a>";
+			imgHTML += "</li>";
+
+			$(".char-list-thum>ul").append(imgHTML);
+		}
+	}
+
+	charcterthum();
 });
