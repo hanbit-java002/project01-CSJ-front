171daed8377979ee6e94b2c97f656a91a2e69c28
Cyphers <wntjdco92@naver.com>
2017-03-21 19:29:17 +0900

    20170320 메인의 왼쪽 배너 기능 추가, 오른쪽아래 배너 기능 추가 (미완)

--- src/js/main.js
+++ src/js/main.js
@@ -29,18 +29,23 @@ require([
 		}, 3000);
 
 		$(".pause-btn").on("click", function() {
-			if(flag === true) {
+			if(flag) {
 				clearTimeout(timer);
-				flag = !flag;
+				$(".pause-btn").css("background-position-y", "100%");
 			}
-			else if(flag ===false) {
+			else{
 				timer = setTimeout(function() {
 					rotateImg(++index);
 				}, 3000);
-				flag = !flag;
+				$(".pause-btn").css("background-position-y", "0%");
 			}
+			flag = !flag;
 		});
 	}
+
+	//$(".btn-box>li:first-child").prepend(rotateImg(currentIndex));
+
+	$(".btn-box>li:nth-child(3)").append(mainImgList.length);
 
 	$(".prev-btn").on("click", function() {
 		rotateImg(currentIndex - 1);
@@ -49,5 +54,35 @@ require([
 	$(".next-btn").on("click", function() {
 		rotateImg(currentIndex + 1);
 	});
+
+	$(".toon-thum-block>li").on("mouseenter", function() {
+		$(".selected-toon").css("left", $(this).index() * 51 + 5);
+	});
+
+	var eventimgList = ["main-left-bnr1.jpg", "main-left-bnr2.jpg",
+		"main-left-bnr3.jpg", "main-left-bnr4.jpg"];
+
+	function issueImg(index) {
+		if (!index || index >= eventimgList.length) {
+			index = 0;
+		}
+		else if (index < 0) {
+			index = eventimgList.length - 1;
+		}
+
+		currentIndex = index;
+		var imgIndex = eventimgList[index];
+
+		$(".issue-img").css("background-image", "url('" +
+			global.root + "/img/" + imgIndex + "')");
+		}
+		$(".prev-little-btn").on("click", function() {
+			issueImg(currentIndex - 1);
+		});
+
+		$(".next-little-btn").on("click", function() {
+			issueImg(currentIndex + 1);
+		});
 	rotateImg();
+	issueImg();
 });
